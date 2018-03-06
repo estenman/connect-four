5d605627a661752ec3e17f40bc8754440e58501e
Ericka Stenman <elstenman@gmail.com>
2018-03-05 21:06:13 -0800

    Choose column and add disc

--- scripts/home.js
+++ scripts/home.js
@@ -17,17 +17,34 @@ Circle.prototype = {
     canvas_context.stroke();
     canvas_context.fillStyle = this.discColor;
     canvas_context.fill();
+  },
+  addDisc: function(selectedCol){
+    for(var i = 5; i > 0; i--){
+      if (selectedCol[i].taken == 0 && playersTurn == 1) {
+        var newDisc = new Circle(selectedCol[i].xAxis, selectedCol[i].yAxis, 'black');
+        newDisc.render();
+        selectedCol[i].taken = 1;
+        playersTurn = 2;
+        break;
+      } else if (selectedCol[i].taken == 0 && playersTurn == 2) {
+        var newDisc = new Circle(selectedCol[i].xAxis, selectedCol[i].yAxis, 'red');
+        newDisc.render();
+        selectedCol[i].taken = 2;
+        playersTurn = 1;
+        break;
+      }
+
+
+    }
   }
 }
 
-//Creates instances of the players discs
-var playerOneDisc = new Circle(30, 250, 'black');
-var playerTwoDisc = new Circle(50, 350, 'red');
+//Creates instances of the player discs
+var newDisc = new Circle(0, 0, 'black');
 
-//Renders the players discs
+//Renders the player discs
 function renderCircle(){
-  playerOneDisc.render();
-  playerTwoDisc.render();
+  newDisc.render();
 }
 
 //Line object constructor
@@ -48,8 +65,66 @@ function renderLines(){
   }
 }
 
-
 window.onload = function() {
   renderLines();
-  renderCircle();
 }
+
+//flag for whose turn it is
+var playersTurn = 1;
+
+//Array representing columns
+var column1 = arrayCreator(50);
+var column2 = arrayCreator(150);
+var column3 = arrayCreator(250);
+var column4 = arrayCreator(350);
+var column5 = arrayCreator(450);
+var column6 = arrayCreator(550);
+var column7 = arrayCreator(650);
+
+//Creates arrays for columns
+function arrayCreator (numx){
+  var columnArray = [];
+  var yAxisx = 50;
+  for (var i = 0; i < 6; i++){
+    if(i === 0){
+      var object = {taken:0, xAxis:numx, yAxis:50};
+      columnArray.push(object);
+    } else {
+      var object2 = {taken:0, xAxis:numx, yAxis:0};
+      object2.yAxis = yAxisx+=100;
+      columnArray.push(object2);
+    }
+  }
+  return columnArray;
+};
+
+//Event listeners for column choosen
+var colOneButton = document.getElementById("col-one-button");
+var colTwoButton = document.getElementById("col-two-button");
+var colThreeButton = document.getElementById("col-three-button");
+var colFourButton = document.getElementById("col-four-button");
+var colFiveButton = document.getElementById("col-five-button");
+var colSixButton = document.getElementById("col-six-button");
+var colSevenButton = document.getElementById("col-seven-button");
+
+colOneButton.addEventListener("click", function() {
+  newDisc.addDisc(column1);
+});
+colTwoButton.addEventListener("click", function() {
+  newDisc.addDisc(column2);
+});
+colThreeButton.addEventListener("click", function() {
+  newDisc.addDisc(column3);
+});
+colFourButton.addEventListener("click", function() {
+  newDisc.addDisc(column4);
+});
+colFiveButton.addEventListener("click", function() {
+  newDisc.addDisc(column5);
+});
+colSixButton.addEventListener("click", function() {
+  newDisc.addDisc(column6);
+});
+colSevenButton.addEventListener("click", function() {
+  newDisc.addDisc(column7);
+});
